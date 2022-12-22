1c221d3ec6a797e203113ae98e4eb04f214e74c0
Ubuntu <adem@ademvm.nsxsysszpkqetcpfzhi30nn0gb.parx.internal.cloudapp.net>
2022-12-22 13:05:16 +0000

    started the monitoring phase

--- web-services/test.py
+++ web-services/test.py
@@ -1,4 +1,3 @@
-# import predict
 import requests
 
 ride = {
@@ -6,10 +5,6 @@ ride = {
     "DOLocationID": 50,
     "trip_distance": 40
 }
-
-# features = predict.prepare_features()
-# pre = predict.predict(ride)
-# print(pred)
 
 url = 'http://localhost:9696/predict'
 response = requests.post(url, json=ride)
