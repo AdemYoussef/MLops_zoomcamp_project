b53468259e4ed146dc801c9423940f7aead37e28
Ubuntu <adem@ademvm.nsxsysszpkqetcpfzhi30nn0gb.parx.internal.cloudapp.net>
2022-12-23 08:32:35 +0000

    fixed a miss-specelling word -_-------

--- monitoring/prediction_service/app.py
+++ monitoring/prediction_service/app.py
@@ -31,8 +31,8 @@ def predict():
         'duration' : float(y_pred)
     }
 
-    save_to_db(record, prediction)
-    send_to_evidently_service(record, prediction)
+    save_to_db(record, float(y_pred))
+    send_to_evidently_service(record, float(y_pred))
 
     return jsonify(result)
 
